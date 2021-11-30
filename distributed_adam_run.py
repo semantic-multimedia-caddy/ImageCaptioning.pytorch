b24190c7ff68e034319a59bda869410450e59b00
Lee, Jaeyoung <wayexists02@gmail.com>
2021-11-30 08:19:50 +0000

    distributed_adam_run.py

---
+++ distributed_adam_run.py
@@ -0,0 +1,23 @@
+import argparse
+import torch
+import time
+from torch import multiprocessing as mp
+import captioning.utils.opts as opts
+from distributed_utils import setup, cleanup
+from train_distributed_adam import train
+
+
+def run():
+    opt = opts.parse_opt()
+    # mp.spawn(train, args=(opt, opt.world_size), nprocs=1, join=True)
+
+    now = time.time()
+    train(opt)
+    duration = time.time() - now
+
+    print(f"Duration: {duration} sec(s).")
+    
+    
+if __name__ == "__main__":
+    run()
+    
