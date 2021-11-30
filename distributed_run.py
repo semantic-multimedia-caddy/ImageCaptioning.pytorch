0987246897a7ebede84e7295570c9b7bd5f4b95b
Lee, Jaeyoung <wayexists02@gmail.com>
2021-11-30 07:34:48 +0000

    distributed_run time

--- distributed_run.py
+++ distributed_run.py
@@ -1,5 +1,6 @@
 import argparse
 import torch
+import time
 from torch import multiprocessing as mp
 import captioning.utils.opts as opts
 from distributed_utils import setup, cleanup
@@ -9,7 +10,12 @@ from train_distributed import train
 def run():
     opt = opts.parse_opt()
     # mp.spawn(train, args=(opt, opt.world_size), nprocs=1, join=True)
+
+    now = time.time()
     train(opt)
+    duration = time.time() - now
+
+    print(f"Duration: {duration} sec(s).")
     
     
 if __name__ == "__main__":
